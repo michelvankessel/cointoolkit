40d5573da38d1d11f613bace8d0bfb516241708f
backpacker69 <backpacker69@protonmail.com>
2022-01-21 10:42:23 +1100

    maybe not

--- js/cointoolkit.js
+++ js/cointoolkit.js
@@ -755,7 +755,7 @@ $(document).ready(function() {
 
 							console.log("signature result",result);
 							$.each(result, function(idx,itm) {
-								var signature = Crypto.util.hexToBytes(itm);
+								var signature = Crypto.util.hexToBytes(itm+hashType.toString(16));
 								if (currenttransaction.signmultisig(idx,undefined,Crypto.util.hexToBytes(hashType.toString(16)),signature)) {
 									success=true;
 									}
